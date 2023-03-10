7cab3cf67d06608aa298b1a7d879187f323a9404
mauricio <jourdanmauricio@gmail.com>
2023-03-10 02:35:58 -0300

    Fix Sedders

--- db/seeders/20221130125351-settings.js
+++ db/seeders/20221130125351-settings.js
@@ -3,155 +3,177 @@
 module.exports = {
   async up(queryInterface) {
     return queryInterface.bulkInsert('settings', [
-      // NAV
       {
         type: 'image',
         feature: 'logoImage',
         value:
           'https://res.cloudinary.com/dn7npxeof/image/upload/v1677218204/laura_rodriguez/logo_desktop.svg',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'navBgColor',
         value: '#eff6ff',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'navTextColor',
         value: '#1f2937',
+        comment: null,
       },
       {
         type: 'stcoloryle',
         feature: 'navHoverColor',
         value: '#000000',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'navCurrentPageColor',
         value: '#14b8a6',
+        comment: null,
       },
-      // HERO
       {
         type: 'image',
         feature: 'heroImage',
         value:
           'https://res.cloudinary.com/dn7npxeof/image/upload/v1676950615/laura_rodriguez/home.jpg',
+        comment: null,
       },
       {
         type: 'style',
         feature: 'heroOpacity',
         value: '0.3',
+        comment: null,
       },
       {
         type: 'text',
         feature: 'heroText',
         value:
           '<h1 class="ql-align-center"><strong style="color: rgb(255, 255, 204);">﻿Doctora Laura Rodriguez</strong></h1>',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'heroTextColor',
         value: '#fff',
+        comment: null,
       },
       {
         type: 'style',
         feature: 'heroTop',
         value: '50%',
+        comment: null,
       },
-      // BODY
       {
         type: 'color',
         feature: 'bodyBgColor',
         value: '#f3f4f6',
+        comment: null,
       },
-      // SECTIONS
       {
         type: 'color',
         feature: 'aboutBgColor',
         value: '#f3f4f6',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'servicesBgColor',
         value: '#f3f4f6',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'blogBgColor',
         value: '#f3f4f6',
+        comment: null,
       },
-      // FOOTER
       {
         type: 'color',
         feature: 'footerBgColor',
-        value: '#161716',
+        value: '#1c211c',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'footerTextColor',
         value: '#ffffff',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'footerButtonsColor',
         value: '#ffffff',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'footerButtonsHoverColor',
         value: '#131213',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'footerLinksColor',
         value: '#ffffff',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'footerLinksHoverColor',
         value: '#ffffff',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'footer2BgColor',
         value: '#131213',
+        comment: null,
       },
       {
         type: 'color',
         feature: 'footer2TextColor',
         value: '#fefeff',
+        comment: null,
       },
-      // SOCIAL MEDIA
       {
         type: 'SOCIAL_MEDIA',
         feature: 'instagram',
         value: 'https://www.instagram.com/lalaloberia/',
+        comment: null,
       },
       {
         type: 'SOCIAL_MEDIA',
         feature: 'facebook',
         value: 'https://www.facebook.com/lala.rodriguez.90857/',
+        comment: null,
       },
       {
         type: 'SOCIAL_MEDIA',
         feature: 'twitter',
         value: '',
+        comment: null,
       },
       {
         type: 'CONTACT',
         feature: 'whatsapp',
         value:
           'https://wa.me/5492262470100?text=Hola, quiero recibir información',
+        comment: null,
       },
       {
         type: 'CONTACT',
         feature: 'email',
         value: 'laurarodriguez@gmail.com',
+        comment: null,
       },
       {
         type: 'CONTACT',
         feature: 'phone',
         value: '(02262) 15 470100',
+        comment: null,
       },
       // {
       //   type: 'STYLE',
--- db/seeders/20230301152854-section.js
+++ db/seeders/20230301152854-section.js
@@ -55,10 +55,12 @@ module.exports = {
       'subsections',
       [
         {
-          image: '',
-          alt_image: '',
+          image:
+            'https://res.cloudinary.com/dn7npxeof/image/upload/v1678061985/laura_rodriguez/x-ray-of-the-jaw-2416944_640.jpg',
+          alt_image: 'Ginecología',
           name: 'Ginecología',
-          content: '<p>Servicios de ginecología</p>',
+          content:
+            '<p>Existen una amplia variedad de tratamientos ginecológicos, algunos de los cuales son:</p><p><br></p><ul><li> Exámenes de rutina: los exámenes de rutina, como el examen pélvico y el examen de Papanicolaou, son importantes para detectar afecciones ginecológicas temprano y para prevenir enfermedades.</li></ul><p><br></p><ul><li> Tratamiento de infecciones: las infecciones vaginales, como la vaginosis bacteriana y la candidiasis, pueden tratarse con medicamentos antifúngicos o antibióticos.</li></ul><p><br></p><ul><li> Tratamiento de enfermedades de transmisión sexual: las enfermedades de transmisión sexual, como la clamidia, la gonorrea y la sífilis, pueden tratarse con antibióticos.</li></ul><p><br></p><ul><li> Terapia hormonal: la terapia hormonal puede ser útil para tratar síntomas relacionados con la menopausia, como sofocos, sudores nocturnos y sequedad vaginal.</li></ul><p><br></p><p>Es importante que las mujeres visiten regularmente a un ginecólogo para detectar cualquier problema de salud ginecológica temprano y recibir tratamiento adecuado.</p>',
           section_id: service_id,
           created_at: new Date(),
           updated_at: new Date(),
@@ -70,10 +72,12 @@ module.exports = {
       'subsections',
       [
         {
-          image: '',
-          alt_image: '',
+          image:
+            'https://res.cloudinary.com/dn7npxeof/image/upload/v1677275330/laura_rodriguez/home_2%20_opt.jpg',
+          alt_image: 'Sexología',
           name: 'Sexología',
-          content: '<p>Servicios de sexología</p>',
+          content:
+            '<p>La sexualidad es una parte importante de la vida de los adolescentes y es fundamental que reciban educación sexual y apoyo para desarrollar una sexualidad saludable y positiva. La sexología en la adolescencia se enfoca en ayudar a los jóvenes a entender su cuerpo, sus emociones y sus relaciones, y a tomar decisiones informadas y responsables.</p><p><br></p><p>En la adolescencia, la identidad de género y la orientación sexual pueden ser temas particularmente importantes. La sexología en la adolescencia puede ayudar a los jóvenes a comprender y aceptar su identidad de género y orientación sexual, y a lidiar con la discriminación y el estigma asociados con ciertas identidades sexuales.</p><p><br></p><p>La sexología en la adolescencia y el género son temas importantes para abordar y comprender para ayudar a los jóvenes a desarrollar una sexualidad saludable y positiva, y para promover la igualdad de género y la comprensión de las diferencias sexuales y de género.</p>',
           section_id: service_id,
           created_at: new Date(),
           updated_at: new Date(),
@@ -85,10 +89,12 @@ module.exports = {
       'subsections',
       [
         {
-          image: '',
-          alt_image: '',
+          image:
+            'https://res.cloudinary.com/dn7npxeof/image/upload/v1676996166/laura_rodriguez/profile%20_opt.jpg',
+          alt_image: 'Tratamientos laser',
           name: 'Tratamientos laser',
-          content: '<p>Tratamientos laser</p>',
+          content:
+            '<p>Los tratamientos láser ginecológicos son una opción cada vez más popular para abordar diferentes afecciones en el área vaginal y vulvar, tales como la incontinencia urinaria, la sequedad vaginal, la dispareunia, las cicatrices de episiotomía y la atrofia vaginal.</p><p><br></p><p>El tratamiento láser funciona mediante la aplicación de un haz de luz de alta energía en la zona a tratar, lo que estimula la producción de colágeno y elastina y mejora la irrigación sanguínea. Esto a su vez ayuda a mejorar la calidad del tejido y a reducir los síntomas de las diferentes afecciones.</p><p><br></p><p>Uno de los tratamientos más comunes es la terapia láser vaginal, que se utiliza para tratar la sequedad vaginal, la incontinencia urinaria y otros síntomas relacionados con la menopausia. El procedimiento es rápido y prácticamente indoloro. La paciente puede volver a sus actividades normales inmediatamente después del tratamiento.</p><p><br></p><p>Otro tratamiento láser ginecológico es el rejuvenecimiento vulvar, que se enfoca en mejorar la apariencia de la vulva y reducir los síntomas de la atrofia vulvar. También es un procedimiento rápido y seguro que se realiza en el consultorio médico.</p><p><br></p><p>En general, los tratamientos láser ginecológicos son muy seguros y efectivos, y pueden mejorar significativamente la calidad de vida de las mujeres que sufren diferentes afecciones en la zona vaginal y vulvar.&nbsp;</p>',
           section_id: service_id,
           created_at: new Date(),
           updated_at: new Date(),
