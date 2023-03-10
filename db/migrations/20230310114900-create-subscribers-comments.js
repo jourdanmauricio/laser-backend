94fa42d1b4c5e3c4f7f7b68528bfbadae129b731
mauricio <jourdanmauricio@gmail.com>
2023-03-10 12:44:53 -0300

    add subscribers

---
+++ db/migrations/20230310114900-create-subscribers-comments.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const { DataTypes, Sequelize } = require('sequelize');
+const { SUBSCRIBER_TABLE } = require('./../models/subscriber.model');
+const { COMMENT_TABLE } = require('./../models/comment.model');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.createTable(SUBSCRIBER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      status: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      reason: {
+        allowNull: true,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'updated_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
+    await queryInterface.createTable(COMMENT_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      phone: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      comment: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'updated_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable(SUBSCRIBER_TABLE);
+    await queryInterface.dropTable(COMMENT_TABLE);
+  },
+};
